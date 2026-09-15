7f063d43362f067a3c4479d9befa4b084189bc2a
enzoudev <enzoroot6@gmail.com>
2026-09-14 23:19:19 -0300

    fix: corrige controller de usuários

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Body, Post } from '@nestjs/common';
 import { UsersService } from './users.service.js';
+import { CreateUserDto } from './DTO/create-user.dto.js';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly userService: UsersService){}
 
-    @Post
+    @Post()
     create(@Body() createUserDto: CreateUserDto){
 
         return this.userService.create(createUserDto)
