aee6e61e76b4ee5899563c47521b7ff0b79e6b04
enzoudev <enzoroot6@gmail.com>
2026-09-14 23:18:44 -0300

    feat: implementa criação de usuários

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './DTO/create-user.dto.js';
+import { PrismaService } from '../prisma/prisma.service.js';
+import { ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    
+    constructor (private readonly prisma: PrismaService) {}
+
+
+    async create (createUserDto: CreateUserDto) {
+        try {
+
+
+            const hashedPassword = await bcrypt.hash(createUserDto.password, 10)
+
+            const user = await this.prisma.user.create({
+                data: {name: createUserDto.name,
+                email: createUserDto.email,
+                password: hashedPassword}
+            })
+
+            return user
+        }
+
+        catch(err: any){
+            if(err.code === "P2002") {
+                throw new ConflictException("Esse e-mail já está cadastrado")
+            }
+
+            throw err
+        }
+    }
+}
