ac9e0b1fd2e383a40dc2804e928fa6c9ef75ea47
enzoudev <enzoroot6@gmail.com>
2026-09-17 22:58:34 -0300

    feat: adiciona endpoint para criação de salas

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { RoomsService } from './rooms.service.js';
+import { CreateRoomDto } from './DTO/create-room.dto.js';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+
+    constructor(private readonly roomsService: RoomsService) {}
+
+    @Post()
+    create(@Body() createRoomDto: CreateRoomDto){
+        return this.roomsService.create(createRoomDto)
+    }
+}
