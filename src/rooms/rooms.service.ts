b57f7df4a642755f9a90f1de045ff645321d46e1
enzoudev <enzoroot6@gmail.com>
2026-09-18 23:58:33 -0300

    feat: implementa criação de salas

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service.js';
+import { CreateRoomDto } from './DTO/create-room.dto.js';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+    constructor(private readonly prisma: PrismaService) {}
+
+     async create (createRoomDto: CreateRoomDto) {
+            try {
+    
+    
+                
+    
+                const user = await this.prisma.user.create({
+                    data: {name: createRoomDto.name,
+                    }
+                })
+    
+                return user
+            }
+    
+            catch(err: any){
+                if(err.code === "P2002") {
+                    throw new ConflictException("Esse e-mail já está cadastrado")
+                }
+    
+                throw err
+            }
+        }
+
+}
